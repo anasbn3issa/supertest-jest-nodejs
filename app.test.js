0fbeef10d8cec7f806f41bc0f9719edf6deb4616
anasbn3issa <anasben3issa@gmail.com>
2022-05-10 11:08:20 +0100

    added 2nd basic test running successfully

--- app.test.js
+++ app.test.js
@@ -2,7 +2,6 @@
 const request = require('supertest')
 const app = require('./app.js')
 describe("POST /users", () => {
-
     describe("when passed a username and password", () => {
         test("should respond with a 200 status code", async () => {
           const response = await request(app).post("/users").send({ 
@@ -13,11 +12,18 @@ describe("POST /users", () => {
         })
       })
 
-  describe("when the username or password is missing", () => {
-  // should return a 400 status code to show there was a user error.
-  // should return a json object that contains an error message.
-  // should specify json as the content type in the http header.
-  })
+    describe("when the username or password is missing", () => {
+        test("should return a 400 status code", async () => {
+            const bodies = [
+            { username: "username" },
+            { password: "password" }
+            ]
+            for (const body of bodies) {
+            const response = await request(app).post("/users").send(body)
+            expect(response.statusCode).toBe(400)
+            }
+        })
+    })
 })
 
 
